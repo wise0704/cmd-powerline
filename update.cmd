c4a701bec53c29978d2fa703b09b603c2306174e
Daniel Jeong <wise0704@outlook.com>
2023-02-05 04:48:12 +0100

    Don't turn off echo in update.cmd

--- update.cmd
+++ update.cmd
@@ -1,22 +1,22 @@
-@echo off
-%*
-setlocal EnableDelayedExpansion
-prompt
-for /l %%i in (0,1,%PL_I%) do (
+@%*
+
+@setlocal EnableDelayedExpansion
+@prompt
+@for /l %%i in (0,1,%PL_I%) do @(
     if defined PL_V[%%i] (
         call :invoke_and_append %%i
     ) else (
         prompt !PROMPT!!PL_P[%%i]!
     )
 )
-endlocal & call prompt %PROMPT%
-exit /b
+@endlocal & call prompt %PROMPT%
+@exit /b
 
 :invoke_and_append (index) -> var, cmd, text, PROMPT
-    set "var=!PL_V[%1]!"
-    set "cmd=!PL_C[%1]!"
-    set "text=!PL_P[%1]!"
-    for /f %var% in (%cmd%) do (
+    @set "var=!PL_V[%1]!"
+    @set "cmd=!PL_C[%1]!"
+    @set "text=!PL_P[%1]!"
+    @for /f %var% in (%cmd%) do @(
         prompt %PROMPT%%text%
     )
-    goto :eof
+    @goto :eof
